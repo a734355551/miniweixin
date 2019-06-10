0c56664d9a0b0a3639cc1be2cf73e37edbd5e91c
a734355551 <734355551@qq.com>
2019-06-10 16:26:59 +0800

    feat:radio组件使用

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -180,44 +180,70 @@ Page({
     // videoUtil.uploadVideo();
     // 以下是原来的代码，不删除，便于参照
     var me = this;
-
     wx.chooseVideo({
       sourceType: ['album'],
-      success: function (res) {
-        console.log(res);
+      success(res) {
+        console.log(res)
+        var duration = res.duration
+        var tmpHeight = res.height
+        var tmpWidth = res.width
+        var tmpVideoUrl = res.tempFilePath
+        var tmpCoverUrl = res.thumbTempFilePath
 
-        var duration = res.duration;
-        var tmpHeight = res.height;
-        var tmpWidth = res.width;
-        var tmpVideoUrl = res.tempFilePath;
-        var tmpCoverUrl = res.thumbTempFilePath;
-
-        if (duration > 11) {
+        if(duration > 16){
           wx.showToast({
-            title: '视频长度不能超过10秒...',
+            title: '视频长度不能超过15s',
+            icon:"none",
+            duration:2500
+          })
+        } else if (duration < 1){
+          wx.showToast({
+            title: '视频长度太短,请上传超过1s的视频',
             icon: "none",
             duration: 2500
           })
-        } else if (duration < 1) {
-          wx.showToast({
-            title: '视频长度太短，请上传超过1秒的视频...',
-            icon: "none",
-            duration: 2500
-          })
-        } else {
-          // 打开选择bgm的页面
-          wx.navigateTo({
-            url: '../chooseBgm/chooseBgm?duration=' + duration
-            + "&tmpHeight=" + tmpHeight
-            + "&tmpWidth=" + tmpWidth
-            + "&tmpVideoUrl=" + tmpVideoUrl
-            + "&tmpCoverUrl=" + tmpCoverUrl
-            ,
-          })
+        }else{
+          //TODO 打开选择bgm的页面
         }
-
       }
     })
+    // wx.chooseVideo({
+    //   sourceType: ['album', 'camera'],
+    //   success: function (res) {
+    //     console.log(res);
+
+    //     var duration = res.duration;
+    //     var tmpHeight = res.height;
+    //     var tmpWidth = res.width;
+    //     var tmpVideoUrl = res.tempFilePath;
+    //     var tmpCoverUrl = res.thumbTempFilePath;
+
+    //     if (duration > 11) {
+    //       wx.showToast({
+    //         title: '视频长度不能超过10秒...',
+    //         icon: "none",
+    //         duration: 2500
+    //       })
+    //     } else if (duration < 1) {
+    //       wx.showToast({
+    //         title: '视频长度太短，请上传超过1秒的视频...',
+    //         icon: "none",
+    //         duration: 2500
+    //       })
+    //     } else {
+    //       // 打开选择bgm的页面
+    //       wx.navigateTo({
+    //         url: '../chooseBgm/chooseBgm?duration=' + duration
+    //         + "&tmpHeight=" + tmpHeight
+    //         + "&tmpWidth=" + tmpWidth
+    //         + "&tmpVideoUrl=" + tmpVideoUrl
+    //         + "&tmpCoverUrl=" + tmpCoverUrl
+    //         ,
+    //       })
+    //     }
+
+    //   }
+    // })
 
   },
 
